8dcf577c7d6dbab301071243154e3f7985b5d644
maruizhe <maruizhe@live.com>
2022-12-21 00:32:23 +0800

    wrote a readme file

---
+++ Task1/task1.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+
+def cv_show(name,img):
+    cv2.imshow(name,img)
+    cv2.waitKey(0)
+
+img = cv2.imread('./before.png')
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+lower_red = np.array([0,43,46])
+upper_red = np.array([11,255,255])
+
+mask = cv2.inRange(hsv, lower_red, upper_red)
+
+res = cv2.bitwise_and(img, img, mask = mask)
+
+# cv_show ('res/hsv/res', np.hstack((img, hsv, res)))
+
+res1 = cv2.cvtColor(res, cv2.COLOR_HSV2BGR)
+gray = cv2.cvtColor(res1, cv2.COLOR_BGR2GRAY)
+# cv_show('bgr', gray)
+
+erode = cv2.erode(gray, None, iterations = 1)
+dilate = cv2.dilate(erode, None, iterations = 1)
+# cv_show('dilate', dilate)
+
+ret, thresh = cv2.threshold(dilate, 20, 255, cv2.THRESH_BINARY)
+# cv_show('thresh', thresh)
+
+contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+cnt = contours[0]
+
+x, y, w, h = cv2.boundingRect(cnt)
+img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+cv_show('img', img)
